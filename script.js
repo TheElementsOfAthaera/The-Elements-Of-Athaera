fe4bf06e4d701b5d87589a7cf369a5cc625e18cb
AHJS93 <123779946+AHJS93@users.noreply.github.com>
2025-06-09 04:54:54 -0500

    Fixed images, added Javascript to handle banner

---
+++ script.js
@@ -0,0 +1,16 @@
+var picPaths = ['images/SacredFlameBanner.png', 'images/SanguineEmbraceBanner.png']; // Array of image paths
+  var curPic = 0; // Counter for current image index
+
+  function swapImage() {
+    var img = document.getElementById('banner');
+    img.style.opacity = 0;
+    setTimeout(function() {
+        curPic = (curPic + 1) % picPaths.length;
+        img.src = picPaths[curPic];
+        img.style.opacity = 1; // Fade in new image
+      }, 500);
+  }
+
+  window.onload = function() {
+    setInterval(swapImage, 5000); // Call swapImage() every 5 seconds
+  }
